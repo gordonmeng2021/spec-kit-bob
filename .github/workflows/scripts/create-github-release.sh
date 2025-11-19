950686cf15a383b539ff1663ba4a12ba8a1b1f2e
Meng Hin Lok Gordon <96650399+gordonmeng2021@users.noreply.github.com>
2025-11-19 14:23:53 +0800

    Update create-github-release.sh

--- .github/workflows/scripts/create-github-release.sh
+++ .github/workflows/scripts/create-github-release.sh
@@ -23,6 +23,7 @@ gh release create "$VERSION" \
   .genreleases/spec-kit-template-gemini-sh-"$VERSION".zip \
   .genreleases/spec-kit-template-gemini-ps-"$VERSION".zip \
   .genreleases/spec-kit-template-cursor-agent-sh-"$VERSION".zip \
+  .genreleases/spec-kit-template-bob-agent-sh-"$VERSION".zip \
   .genreleases/spec-kit-template-cursor-agent-ps-"$VERSION".zip \
   .genreleases/spec-kit-template-opencode-sh-"$VERSION".zip \
   .genreleases/spec-kit-template-opencode-ps-"$VERSION".zip \
